4991995ca4f1e60cbf667adf9a8fd49076566ddf
Mark Varga <varga.mrk@gmail.com>
2021-11-14 23:04:11 +0100

    Add status display

---
+++ evaluate.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def largest_tile(state):
+    return np.max(state)
+
+def summary(stats) -> str:
+    '''Takes dictionary in form of largest_tile:n_achieved'''
+    s = ''
+    n_episodes = sum(stats.values())
+    maxtiles_sorted_by_key = [(k, v) for k,v in sorted(stats.items(), key=lambda x: x[0])]
+    accumulated_achieved = {}
+    for highest in sorted(stats.keys(), reverse=True):
+        s += '{}:\t{}\n'.format(highest, stats[highest])
+    return s
