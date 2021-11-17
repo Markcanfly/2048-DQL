ca783d3252a98e7b8a9ff3a5617dc47a82e81bc8
Mark Varga <varga.mrk@gmail.com>
2021-11-17 20:10:59 +0100

    Better % repr

--- evaluate.py
+++ evaluate.py
@@ -9,6 +9,13 @@ def summary(stats) -> str: # TODO better summary
     n_episodes = sum(stats.values())
     maxtiles_sorted_by_key = [(k, v) for k,v in sorted(stats.items(), key=lambda x: x[0])]
     accumulated_achieved = {}
+    for highscore in stats:
+        accumulated_score = 0
+        for highscore_ in stats:
+            if highscore_ < highscore:
+                accumulated_score += stats[highscore_]
+        accumulated_achieved[highscore] = accumulated_score
+    s += 'Score\tN_achieved\t%acc_achieved'
     for highest in sorted(stats.keys(), reverse=True):
-        s += '{}:\t{}\n'.format(highest, stats[highest])
+        s += f'{highest}:\t{stats[highest]}\t{round(accumulated_achieved[highest]/n_episodes,2)*100}%\n'
     return s
